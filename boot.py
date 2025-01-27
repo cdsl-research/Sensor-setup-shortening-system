bd43e2f431042d72729548d6995b9a30202c32b3
C0A22016 <107175276+C0A22016@users.noreply.github.com>
2025-01-27 18:20:54 +0900

    Update boot.py

--- boot.py
+++ boot.py
@@ -3,13 +3,13 @@ import utime
 import network
 import webrepl
 import get_sensor_info
+import send
+
 
 #　WIFI設定を記述
 SSID_NAME = "*****"
 SSID_PASS = "*****"
 REPL_PASS = "1234"
-
-file_info = dict()
 
 
 def connect_wifi(ssid, passkey, timeout=10):
@@ -39,27 +39,16 @@ def connect_wifi(ssid, passkey, timeout=10):
         print('Connection failed!')
         return ''
     
-    
-def send_sensor_info(sensor_info_dict):
-    try:
-        headers = {"Content-Type": "application/json"}
-
-        # POSTリクエスト送信
-        response = urequests.post(SERVER_URL, headers=headers, data=ujson.dumps(data))
-        
-        # レスポンス確認
-        print("Response status:", response.status_code)
-        print("Response text:", response.text)
-        response.close()
-    except Exception as e:
-        print("Failed to send data:", e)
-
-
-if __name__ == '__main__':
+   
+def main():
     wifi= network.WLAN(network.STA_IF)
     if wifi.isconnected():
         connect_wifi(SSID_NAME, SSID_PASS)
     else:
         connect_wifi(SSID_NAME, SSID_PASS)
-        #file_info = get_sensor_info.main()
-        
+        send.send_start_time()
+        file_info = get_sensor_info.main()
+
+
+if __name__ == '__main__':
+    main()
